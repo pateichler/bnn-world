3aaf505bf535b3b29b1979363e71f668b6675a65
Patrick Eichler <pateichler12@gmail.com>
2023-10-30 17:29:30 -0500

    Fixed and tested DNAHelper methods

--- src/main/java/com/pat_eichler/bnn/world/DNAHelper.java
+++ src/main/java/com/pat_eichler/bnn/world/DNAHelper.java
@@ -2,11 +2,17 @@ package com.pat_eichler.bnn.world;
 
 import com.pat_eichler.bnn.brain.DNA;
 
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.util.Arrays;
 import java.util.Random;
 
 public class DNAHelper {
+
     public static DNA crossDNA(DNA dna1, DNA dna2, double fitRatio, Random rand, WorldSettings settings){
-        //TODO: Test method
+        return crossDNA(dna1, dna2, fitRatio, rand, settings.MUTATION_RATE, settings.CROSSOVER_RATE);
+    }
+
+    public static DNA crossDNA(DNA dna1, DNA dna2, double fitRatio, Random rand, double mutationRate, double crossoverRate){
         if(dna1.data.length != dna2.data.length)
             throw new RuntimeException("DNA not same length! Can't cross DNA");
 
@@ -15,19 +21,15 @@ public class DNAHelper {
 
         for(int i = 0; i < dna1.data.length; i++){
             int b1 = dna1.data[i], b2 = dna2.data[i], bNew = 0;
-            for (int b = 0; b < 8; b++){
-                if(rand.nextFloat() < settings.MUTATION_RATE && rand.nextFloat() > 0.5f)
-                    bNew += 1;
-                else{
-                    if(rand.nextFloat() < settings.CROSSOVER_RATE)
-                        use1 = !use1;
-                    bNew += (use1 ? b1 : b2) & 1;
-                }
-
-                b1 = b1<<1;
-                b2 = b2<<1;
+            for (int b = 0; b < 8; b++, b1>>=1, b2>>=1){
                 bNew = bNew<<1;
+                if(rand.nextFloat() < crossoverRate)
+                    use1 = !use1;
+
+                int bit = (use1 ? b1 : b2) & 1;
+                bNew += rand.nextFloat() < mutationRate ? bit ^ 1 : bit;
             }
+            data[i] = (byte) bNew;
         }
 
         return new DNA(data);
@@ -35,20 +37,29 @@ public class DNAHelper {
 
     public static double calculateGenePoolVariation(DNA[] genePool){
         //TODO: Test method
-        float[] averageBits = new float[genePool[0].data.length * 8];
+        int[] averageBits = new int[genePool[0].data.length * 8];
         for(DNA dna : genePool){
             for(int i = 0; i < dna.data.length; i++){
-                for(int b = dna.data[i]; b > 0; b=b<<1){
-                    averageBits[i * 8 + b] += b & 1;
+                for(int b = dna.data[i], bVal = 0; b > 0; b=b>>1, bVal++){
+                    averageBits[i * 8 + bVal] += b & 1;
+                }
+            }
+        }
+
+        float[] error = new float[genePool[0].data.length * 8];
+        for(DNA dna : genePool){
+            for(int i = 0; i < dna.data.length; i++){
+                for(int b = dna.data[i], bVal = 0; bVal < 8; b=b>>1, bVal++){
+                    float diff = (float) averageBits[i * 8 + bVal] / genePool.length - (b & 1);
+                    error[i * 8 + bVal] += diff * diff;
                 }
             }
         }
 
         double total = 0d;
-        for (float averageBit : averageBits) {
-            float diff = 0.5f - averageBit / genePool.length;
-            total += diff * diff;
-        }
-        return total;
+        for (float e : error)
+            total += e;
+
+        return total/error.length;
     }
 }
---
+++ src/test/java/com/pat_eichler/bnn/world/TestDNAHelper.java
@@ -0,0 +1,85 @@
+package com.pat_eichler.bnn.world;
+
+import com.pat_eichler.bnn.brain.DNA;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class TestDNAHelper {
+    @Test
+    void testCrossDNANone(){
+        Random r = new Random(){ @Override public float nextFloat() {return  1;} };
+        DNA dna1 = new DNA(new byte[]{(byte) 0x00, (byte) 0x00});
+        DNA dna2 = new DNA(new byte[]{(byte) 0xFF, (byte) 0xFF});
+
+        DNA newDNA = DNAHelper.crossDNA(dna1, dna2, 0, r, 0.5d, 0.5d);
+        byte[] expectedData = new byte[]{(byte) 0x00, (byte) 0x00};
+        Assertions.assertArrayEquals(expectedData, newDNA.data);
+    }
+
+    @Test
+    void testCrossDNAHalfCross(){
+        Random r = new Random(){
+            boolean f = false;
+            @Override
+            public float nextFloat() {
+                f = !f;
+                return f ? 0 : 1;
+            }
+        };
+        DNA dna1 = new DNA(new byte[]{(byte) 0x00, (byte) 0x00});
+        DNA dna2 = new DNA(new byte[]{(byte) 0xFF, (byte) 0xFF});
+
+        DNA newDNA = DNAHelper.crossDNA(dna1, dna2, 0, r, 0.5d, 0.5d);
+        byte[] expectedData = new byte[]{(byte) 170, (byte) 170};
+        Assertions.assertArrayEquals(expectedData, newDNA.data);
+    }
+
+    @Test
+    void testCrossDNAFullMutate(){
+        Random r = new Random(){
+            boolean f = false;
+            @Override
+            public float nextFloat() {
+                f = !f;
+                return f ? 1 : 0;
+            }
+        };
+        DNA dna1 = new DNA(new byte[]{(byte) 0x00, (byte) 0x00});
+        DNA dna2 = new DNA(new byte[]{(byte) 0x00, (byte) 0x00});
+
+        DNA newDNA = DNAHelper.crossDNA(dna1, dna2, 0, r, 0.5d, 0.5d);
+        byte[] expectedData = new byte[]{(byte) 0xFF, (byte) 0xFF};
+        Assertions.assertArrayEquals(expectedData, newDNA.data);
+    }
+
+    @Test
+    void testCalculateGenePoolVariationNone(){
+        //TODO: Complete
+        byte[][] poolData = new byte[][]{
+                {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}
+        };
+        DNA[] genePool = new DNA[poolData.length];
+        for (int i = 0; i < genePool.length; i++)
+            genePool[i] = new DNA(poolData[i]);
+
+        Assertions.assertEquals(0, DNAHelper.calculateGenePoolVariation(genePool));
+    }
+
+    @Test
+    void testCalculateGenePoolVariationSimple(){
+        //TODO: Complete
+        byte[][] poolData = new byte[][]{
+                {0b00000001},
+                {0b00001001},
+                {0b00100001},
+                {0b00001001},
+        };
+        DNA[] genePool = new DNA[poolData.length];
+        for (int i = 0; i < genePool.length; i++)
+            genePool[i] = new DNA(poolData[i]);
+
+        Assertions.assertEquals((0.25*0.25*3 + 0.75*0.75 + 0.5*0.5*2 + 0.5*0.5*2)/8, DNAHelper.calculateGenePoolVariation(genePool));
+    }
+}
